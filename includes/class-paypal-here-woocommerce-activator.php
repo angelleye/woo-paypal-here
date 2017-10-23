50fa7eae0f43910a0334664be2ecb60f9950a8f7
WORKSTATION\andre <andrew@angelleye.com>
2017-10-22 19:53:27 -0500

    Initial commit

---
+++ includes/class-paypal-here-woocommerce-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.angelleye.com
+ * @since      1.0.0
+ *
+ * @package    Paypal_Here_Woocommerce
+ * @subpackage Paypal_Here_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Paypal_Here_Woocommerce
+ * @subpackage Paypal_Here_Woocommerce/includes
+ * @author     Angell EYE <service@angelleye.com>
+ */
+class Paypal_Here_Woocommerce_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-paypal-here-woocommerce-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.angelleye.com
+ * @since      1.0.0
+ *
+ * @package    Paypal_Here_Woocommerce
+ * @subpackage Paypal_Here_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Paypal_Here_Woocommerce
+ * @subpackage Paypal_Here_Woocommerce/includes
+ * @author     Angell EYE <service@angelleye.com>
+ */
+class Paypal_Here_Woocommerce_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
