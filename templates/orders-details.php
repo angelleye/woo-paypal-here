96b1e87527385a12b3aea76350d7ef32571d99da
kcppdevelopers <kcppdevelopers@gmail.com>
2018-01-09 14:10:42 +0530

    Web App - Order Screen, ref #7

--- templates/orders-details.php
+++ templates/orders-details.php
@@ -5,12 +5,11 @@
         ?>
         <div class="col">
             <div class="form-group">
-                 <h2 class="text-center text-primary"><?php echo $this->order->get_formatted_order_total(); ?></h2>
+                <h2 class="text-center text-primary"><?php echo $this->order->get_formatted_order_total(); ?></h2>
             </div>
             <div class="form-group">
                 <a class="btn btn-primary" href="<?php echo esc_url(add_query_arg('actions', 'view_products', remove_query_arg('order_id'))); ?>" role="button">&plus; Add Item</a>
             </div>
-            <br>
             <table class="table ">
                 <tbody>
                     <?php
@@ -29,14 +28,20 @@
                             echo '</tr>';
                         endif;
                     endforeach;
+
+                    if (!in_array('discount', $this->order->get_order_item_totals())) :
+                        echo '<tr>';
+                        echo '<td class="paypal_here_discount">' . 'Discount:' . '</th>';
+                        echo '<td>' . $this->order->get_discount_to_display() . '</td>';
+                        echo '</tr>';
+                    endif;
                     ?>
                 </tbody>
-
             </table>
         </div>
-        <?php
-    } else {
-        echo __('No Pending order found');
-    }
-    ?>
+                    <?php
+                } else {
+                    echo __('No Pending order found');
+                }
+                ?>
 </div>
