4a3be3bdbc369235ba52f6deb09e41f09773aa51
kcppdevelopers <kcppdevelopers@gmail.com>
2018-01-18 18:49:55 +0530

    Web App - Dashboard, ref #2

--- templates/default.php
+++ templates/default.php
@@ -1,10 +1,14 @@
 <div class="row">
-<div class="default-ceneter-button">
-    <div><a class="btn btn-primary w195" href="<?php echo esc_url(add_query_arg('actions', 'order_billing')); ?>" role="button"><?php echo __('Create New Order', ''); ?></a></div><br>
-    <?php if( is_user_logged_in()) : ?>
-    <div><a class="btn btn-primary w195" href="<?php echo esc_url(add_query_arg('actions', 'view_pending_orders')); ?>" role="button"><?php echo __('View Pending Orders', ''); ?></a></div>
-    <?php endif; ?>
-</div>
+    <div class="default-ceneter-button">
+        <div><img src="https://www.paypalobjects.com/webstatic/mobile/hob/web/pp_here_flat.png" class="img-fluid" alt="PayPal Here" ></div><br>
+        <div><img src="https://woocommerce.com/wp-content/themes/woo/images/logo-woocommerce@2x.png" class="img-fluid" alt="PayPal Here" ></div><br>
+        <br>
+        <p class="text-secondary">Create and Process WooCommerce Orders Using PayPal Here.</p><br><br>
+        <div><a class="btn btn-primary w195" href="<?php echo esc_url(add_query_arg('actions', 'order_billing')); ?>" role="button"><?php echo __('Create New Order', ''); ?></a></div><br>
+        <div><a class="btn btn-primary w195" href="<?php echo esc_url(add_query_arg('actions', 'view_pending_orders')); ?>" role="button"><?php echo __('View Pending Orders', ''); ?></a></div>
+        <br><br><br>
+        <div><img src="https://www.angelleye.com/wp-content/uploads/2015/06/angelleye-logo-159x43.png" class="img-fluid" alt="Angell EYE" ></div><br>
+    </div>
 </div>    
 
 
