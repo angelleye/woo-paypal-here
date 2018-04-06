3a110ea0717b243852cf4a38731ee8af1f20da2c
kcppdevelopers <kcppdevelopers@gmail.com>
2018-04-06 10:40:17 +0530

    Add item is getting hung up, ref #46

--- includes/class-woo-paypal-here-payment.php
+++ includes/class-woo-paypal-here-payment.php
@@ -369,7 +369,7 @@ class Woo_PayPal_Here_Payment extends WC_Payment_Gateway {
 
     public function paypal_here_call_back_handler() {
         if (!empty($_GET['Type'])) {
-            //$this->add_log('call back response: ' . print_r($_SERVER, true));
+            $this->add_log('call back response: ' . print_r($_SERVER, true));
             $order_id = !empty($_GET['wc_order_id']) ? absint($_GET['wc_order_id']) : '';
             if (empty($order_id)) {
                 $order_id = str_replace($this->invoice_id_prefix, '', wc_clean($_GET['Number']));
